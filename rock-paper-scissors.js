3798e0ca7a2987023f3f8eb3b18d741b2bd7d5a2
dwnorm2 <dwnorm2@gmail.com>
2022-12-29 19:28:24 -0600

    Add selection buttons and adapt JS code to buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,73 +1,88 @@
 //ROCK PAPER SCISSORS
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+let playerSelection;
+
+rockButton.addEventListener("click", () => {
+  playerSelection = "rock";
+  playRound();
+});
+
+paperButton.addEventListener("click", () => {
+  playerSelection = "paper";
+  playRound();
+});
+
+scissorsButton.addEventListener("click", () => {
+  playerSelection = "scissors";
+  playRound();
+});
+
 let playerScore = 0;
 let computerScore = 0;
 let choice = ["rock", "paper", "scissors"];
 
-function intro() {
-  let gameStart = window.confirm(
-    "Start game? If console is not open, please hit cancel, open console, and refresh page"
-  );
-  if (gameStart) {
-    game();
-  }
-}
+// function intro() {
+//   let gameStart;
+//   if (gameStart) {
+//     game();
+//   }
+// }
 
 function getComputerChoice() {
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function getPlayerChoice() {
-  let input = prompt("Rock, Paper, or Scissors?");
-  if (input == null) {
-    intro();
-  }
-  input = input.toLowerCase();
+// function getPlayerChoice() {
+//   let input = prompt("Rock, Paper, or Scissors?");
+//   if (input == null) {
+//     intro();
+//   }
+//   input = input.toLowerCase();
 
-  let check = validateInput(input);
-  while (check == false) {
-    input = prompt("Type rock, paper, or scissors - Check Spelling");
-    if (input == null) {
-      intro();
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
-  }
-  return input;
-}
+//   let check = validateInput(input);
+//   while (check == false) {
+//     input = prompt("Type rock, paper, or scissors - Check Spelling");
+//     if (input == null) {
+//       intro();
+//     }
+//     input = input.toLowerCase();
+//     check = validateInput(input);
+//   }
+//   return input;
+// }
 
-function validateInput(input) {
-  if (choice.includes(input)) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function validateInput(input) {
+//   if (choice.includes(input)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-function game() {
-  // for (let i = 0; i < 5; i++) {
-  //   playRound();
-  // }
-  do {
-    playRound();
-  } while (playerScore < 5 && computerScore < 5);
-  getScore();
-  let playAgain = window.confirm("Play another game?");
-  if (playAgain) {
-    game();
-  } else {
-    intro();
-  }
-}
+// function game() {
+//   // for (let i = 0; i < 5; i++) {
+//   //   playRound();
+//   // }
+//   do {
+//     playRound();
+//   } while (playerScore < 5 && computerScore < 5);
+//   getScore();
+//   let playAgain = window.confirm("Play another game?");
+//   if (playAgain) {
+//     game();
+//   } else {
+//     intro();
+//   }
+// }
 
 function playRound() {
   const computerSelection = getComputerChoice();
-  const playerSelection = getPlayerChoice();
   console.log(`Computer picked ${computerSelection}`);
   //tie
   if (playerSelection === computerSelection) {
     console.log("It's a Tie");
-    // if you don't want to count ties
-    //playRound();
   }
   //rock
   else if (playerSelection === "rock") {
@@ -99,24 +114,22 @@ function playRound() {
       playerScore++;
     }
   }
+  //game stops at 5 points
+  if (playerScore == 5 || computerScore == 5) {
+    getScore();
+  }
 }
 
 function getScore() {
+  //calculate and declare winner:
   if (playerScore === computerScore) {
-    console.log(
-      `It's a tie! ${playerScore} - ${computerScore}. Refresh page to start new game`
-    );
+    console.log(`It's a tie! ${playerScore} - ${computerScore}.`);
   } else if (playerScore > computerScore) {
-    console.log(
-      `You won! ${playerScore} - ${computerScore}. Refresh page to start new game`
-    );
+    console.log(`You won! ${playerScore} - ${computerScore}.`);
   } else {
-    console.log(
-      `You lost! ${playerScore} - ${computerScore}. Refresh page to start new game`
-    );
+    console.log(`You lost! ${playerScore} - ${computerScore}.`);
   }
+  //reset score:
   playerScore = 0;
   computerScore = 0;
 }
-
-intro();
